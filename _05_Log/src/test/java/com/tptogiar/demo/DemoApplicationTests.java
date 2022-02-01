45051fb231a9c86068ee3f708b8f23b946063a25
Tptogiar <2528891112@qq.com>
2022-02-01 23:30:08 +0800

    日志配置

--- _05_Log/src/test/java/com/tptogiar/demo/DemoApplicationTests.java
+++ _05_Log/src/test/java/com/tptogiar/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.tptogiar.demo;
 
+
 import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+
     @Test
     void contextLoads() {
+        logger.trace("trace.");
+        logger.debug("debug..");
+        //SpringBoot 默认是info级别
+        logger.info("info...");
+        logger.warn("warn....");
+        logger.error("error.....");
+
+
     }
 
 }
