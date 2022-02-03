920e7b13f8ed2ee3baa1b773b4e7a0ec12996244
Tptogiar <2528891112@qq.com>
2022-02-03 19:58:03 +0800

    自定义ViewResolver

--- _07_SpringBootCRUD/src/main/java/com/tptogiar/demo/controller/HelloWorldController.java
+++ _07_SpringBootCRUD/src/main/java/com/tptogiar/demo/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.tptogiar.demo.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,19 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  * @date 2022/2/3 - 16:20
  */
-@RestController
+@Controller
 public class HelloWorldController {
 
 
+
+
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello world";
     }
 
 
     @GetMapping("/tomcat.png")
+    @ResponseBody
     public String testStaticResourceAndController(){
         return "tomcat.png controller!";
+    }
+
+    @GetMapping("/testThymeleaf")
+    public String testThymeleaf(){
+        return "index";
     }
 
 
